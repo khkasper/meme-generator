e5f4a7f9737b404af179e9674d65f7109187581e
Kristiano <kristianokasper@gmail.com>
2021-07-10 21:44:39 -0300

    Botoes fire, water e earth

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const textInput = document.querySelector('#text-input');
 const imgInput = document.querySelector('#meme-insert');
+const memeImageContainer = document.querySelector('#meme-image-container');
+const fireBttn = document.querySelector('#fire');
+const waterBttn = document.querySelector('#water');
+const earthBttn = document.querySelector('#earth');
+
+// Adiciona texto do "text-input" dentro do "meme-text"
 
 function memeTxt() {
   textInput.addEventListener('input', () => {
@@ -10,6 +16,7 @@ function memeTxt() {
 memeTxt();
 
 // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
+// Adiciona uma imagem selecionada dentro do "meme-image". Usei o elemento 'img' para
 
 function memeImg() {
   imgInput.addEventListener('change', () => {
@@ -19,3 +26,23 @@ function memeImg() {
 }
 memeImg();
 
+function fire() {
+  fireBttn.addEventListener('click', () => {
+    memeImageContainer.style.border = '3px dashed red';
+  });
+}
+fire();
+
+function water() {
+  waterBttn.addEventListener('click', () => {
+    memeImageContainer.style.border = '5px double blue';
+  });
+}
+water();
+
+function earth() {
+  earthBttn.addEventListener('click', () => {
+    memeImageContainer.style.border = '6px groove green';
+  });
+}
+earth();
