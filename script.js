70456f21f1729ed4399093a1ae57c5c85215a99d
Kristiano <kristianokasper@gmail.com>
2021-07-10 22:07:33 -0300

    Funçoes de adicionar novos memes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const textInput = document.querySelector('#text-input');
 const imgInput = document.querySelector('#meme-insert');
-const memeImageContainer = document.querySelector('#meme-image-container');
+const memeImg = document.querySelector('#meme-image');
+const memeImgContainer = document.querySelector('#meme-image-container');
 const fireBttn = document.querySelector('#fire');
 const waterBttn = document.querySelector('#water');
 const earthBttn = document.querySelector('#earth');
+const meme1 = document.querySelector('#meme-1');
+const meme2 = document.querySelector('#meme-2');
+const meme3 = document.querySelector('#meme-3');
+const meme4 = document.querySelector('#meme-4');
 
 // Adiciona texto do "text-input" dentro do "meme-text"
 
@@ -18,31 +23,58 @@ memeTxt();
 // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
 // Adiciona uma imagem selecionada dentro do "meme-image". Usei o elemento 'img' para
 
-function memeImg() {
+function memeImgAdd() {
   imgInput.addEventListener('change', () => {
-    const memeImgInput = document.querySelector('#meme-image');
-    memeImgInput.src = URL.createObjectURL(imgInput.files[0]);
+    memeImg.src = URL.createObjectURL(imgInput.files[0]);
   });
 }
-memeImg();
+memeImgAdd();
 
 function fire() {
   fireBttn.addEventListener('click', () => {
-    memeImageContainer.style.border = '3px dashed red';
+    memeImgContainer.style.border = '3px dashed red';
   });
 }
 fire();
 
 function water() {
   waterBttn.addEventListener('click', () => {
-    memeImageContainer.style.border = '5px double blue';
+    memeImgContainer.style.border = '5px double blue';
   });
 }
 water();
 
 function earth() {
   earthBttn.addEventListener('click', () => {
-    memeImageContainer.style.border = '6px groove green';
+    memeImgContainer.style.border = '6px groove green';
   });
 }
 earth();
+
+function meme1Add() {
+  meme1.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme1.png';
+  });
+}
+meme1Add();
+
+function meme2Add() {
+  meme2.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme2.png';
+  });
+}
+meme2Add();
+
+function meme3Add() {
+  meme3.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme3.png';
+  });
+}
+meme3Add();
+
+function meme4Add() {
+  meme4.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme4.png';
+  });
+}
+meme4Add();
